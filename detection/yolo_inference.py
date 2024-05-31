5032c63eb0dfd1d79175d6a961920e6b0f01673b
malinamanolache <malinamanolache12@gmail.com>
2024-05-31 21:27:51 +0000

    add python script for yolo inference

---
+++ detection/yolo_inference.py
@@ -0,0 +1,163 @@
+import cv2
+import argparse
+import time
+import glob
+import numpy as np
+import os
+import json
+
+# weights_path = "/home/ia2/License-Plate-Recognition/detection/yolo/yolov4-custom-rodosol_best.weights"
+# config_path = (
+#     "/home/ia2/License-Plate-Recognition/detection/yolo/yolov4-custom-rodosol.cfg"
+# )
+# classes_path = "/home/ia2/License-Plate-Recognition/detection/yolo/obj.rodosol_names"
+
+# CONFIDENCE_THRESHOLD = 0.2
+# NMS_THRESHOLD = 0.4
+# COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
+# class_names = []
+# with open(classes_path, "r") as f:
+#     class_names = [cname.strip() for cname in f.readlines()]
+
+# net = cv2.dnn.readNet(weights_path, config_path)
+# net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+# net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+
+# model = cv2.dnn_DetectionModel(net)
+# model.setInputParams(size=(704, 704), scale=1 / 255, swapRB=True)
+# frame = cv2.imread(
+#     "/home/ia2/License-Plate-Recognition/detection/darknet/data/obj/images/cars-br/img_000002.jpg"
+# )
+
+# classes, scores, boxes = model.detect(frame, CONFIDENCE_THRESHOLD, NMS_THRESHOLD)
+# for classid, score, box in zip(classes, scores, boxes):
+#     color = COLORS[int(classid) % len(COLORS)]
+#     label = "%s : %f" % (class_names[classid], score)
+#     cv2.rectangle(frame, box, color, 2)
+#     cv2.putText(
+#         frame, label, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2
+#     )
+
+# cv2.imwrite("output.png", frame)
+
+
+def detect(
+    model,
+    filename: str,
+    class_names: list,
+    conf_thresh: float = 0.5,
+    nms_thresh: float = 0.5,
+) -> tuple:
+
+    image = cv2.imread(filename)
+    classes, scores, boxes = model.detect(image, conf_thresh, nms_thresh)
+    color = (0, 0, 255)
+
+    result_dict = {}
+    objects = []
+    result_dict["filename"] = filename
+
+    for classid, score, box in zip(classes, scores, boxes):
+        label = "%s : %f" % (class_names[classid], score)
+        cv2.rectangle(image, box, color, 2)
+        cv2.putText(
+            image, label, (box[0], box[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2
+        )
+
+        objects.append(
+            {
+                "class_id": int(classid),
+                "class_name": class_names[classid],
+                "box": box.tolist(),
+                "confidence": float(score),
+            }
+        )
+
+    result_dict["objects"] = objects
+
+    return (image, result_dict)
+
+
+def run_detector(
+    model_path: str, input_path: str, image_size: list, save_path: str
+) -> None:
+    # process model paths
+    weights = glob.glob(f"{model_path}/*.weights")[0]
+    classes = glob.glob(f"{model_path}/*.txt")[0]
+    cfg = glob.glob(f"{model_path}/*.cfg")[0]
+
+    # get model
+    net = cv2.dnn.readNet(weights, cfg)
+    net.setPreferableBackend(cv2.dnn.DNN_BACKEND_CUDA)
+    net.setPreferableTarget(cv2.dnn.DNN_TARGET_CUDA_FP16)
+
+    model = cv2.dnn_DetectionModel(net)
+    model.setInputParams(size=image_size, scale=1 / 255, swapRB=True)
+
+    # get classes
+    class_names = []
+    with open(classes, "r") as f:
+        class_names = [cname.strip() for cname in f.readlines()]
+
+    os.makedirs(save_path, exist_ok=True)
+
+    if os.path.isdir(input_path):
+        files = sorted(os.listdir(input_path))
+        results = []
+
+        for file in files:
+            img = cv2.imread(file)
+            img, result = detect(model, img, class_names)
+            results.append(result)
+            
+            image_name = os.path.basename(input_path)
+            cv2.imwrite(os.path.join(save_path, image_name), img)
+
+        json_path = os.path.join(save_path, "result.json")
+        with open(json_path, 'w') as fout:
+            json.dump([result] , fout, indent=2)
+
+    elif os.path.isfile(input_path):
+        img, result = detect(model, input_path, class_names)
+        
+        # save image and result
+        image_name = os.path.basename(input_path)
+        cv2.imwrite(os.path.join(save_path, image_name), img)
+
+        json_path = os.path.join(save_path, "result.json")
+        with open(json_path, 'w') as fout:
+            json.dump([result] , fout, indent=2)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        required=True,
+        help="Directory that contains model config, weights and file with class names.",
+    )
+    parser.add_argument(
+        "--input",
+        type=str,
+        required=True,
+        help="Path to input image or directory of images.",
+    )
+    parser.add_argument(
+        "--image_size",
+        type=tuple,
+        required=False,
+        default=(704, 704),
+        help="Shape of the yolo training images, can be found in the yolo cfg file.",
+    )
+    parser.add_argument(
+        "--out",
+        type=str,
+        required=False,
+        default="/yolo_detections",
+        help="Path to output directory.",
+    )
+    args = parser.parse_args()
+
+    print(args.input)
+    run_detector(args.model_path, args.input, args.image_size, args.out)
